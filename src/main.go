9c0beb8e5a92963be93ed86e16e42cb2572ff573
stigsvensson <erik.hakansson96@gmail.com>
2021-08-14 17:40:12 +0200

    basic repl frame

--- src/main.go
+++ src/main.go
@@ -1,9 +1,80 @@
 package main
-
+ 
 import (
-	"fmt"
+    "bufio"
+    "fmt"
+    "os"
+    "os/exec"
+    "strings"
 )
 
+const cliName string = "sqlitego"
+
+func printPrompt() {
+    fmt.Print(cliName, "> ")
+}
+ 
+func printUnknown(text string) {
+    fmt.Println(text, ": command not found")
+}
+
+func displayHelp() {
+    fmt.Printf(
+        "Welcome to %v! These are the available commands: \n",
+        cliName,
+    )
+    fmt.Println("help    - Show available commands")
+    fmt.Println("clear   - Clear the terminal screen")
+    fmt.Println("exit    - Closes your connection to <dbname>")
+}
+ 
+// clearScreen clears the terminal screen
+func clearScreen() {
+    cmd := exec.Command("clear")
+    cmd.Stdout = os.Stdout
+    cmd.Run()
+}
+
+// handleInvalidCmd attempts to recover from a bad command
+func handleInvalidCmd(text string) {
+    defer printUnknown(text)
+}
+ 
+// handleCmd parses the given commands
+func handleCmd(text string) {
+    handleInvalidCmd(text)
+}
+
+// cleanInput preprocesses input to the db repl
+func cleanInput(text string) string {
+    output := strings.TrimSpace(text)
+    output = strings.ToLower(output)
+    return output
+}
+
 func main() {
-	fmt.Println("Hello world!")
+    // Hardcoded repl commands
+    commands := map[string]interface{}{
+        "help":  displayHelp,
+        "clear": clearScreen,
+    }
+    // Begin the repl loop
+    reader := bufio.NewScanner(os.Stdin)
+    printPrompt()
+    for reader.Scan() {
+        text := cleanInput(reader.Text())
+        if command, exists := commands[text]; exists {
+            // Call a hardcoded function
+            command.(func())()
+        } else if strings.EqualFold("exit", text) {
+            // Close the program on the exit command
+            return
+        } else {
+            // Pass the command to the parser
+            handleCmd(text)
+        }
+        printPrompt()
+    }
+    // Print an additional line if we encountered an EOF character
+	fmt.Println()
 }
